791fdb37ff278b53a6e13fd7bb6d219ad04abfaf
mouwaficbdr <badaroumouwafic@gmail.com>
2025-06-03 21:41:08 +0100

    Refactor la gestion des routes en déplaçant la logique vers le composant AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
 
 import AuthProvider from "./features/auth/AuthProvider";
-
-import Home from "./pages/Home";
-import Dashboard from "./pages/Dashboard";
-import Settings from "./pages/Settings";
 
 const App = () => {
   return (
     <AuthProvider>
-      <Router>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="settings" element={<Settings />} />
-        </Routes>
-      </Router>
+      <AppRoutes />
     </AuthProvider>
   );
 };
