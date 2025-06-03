29f27b97f347868299a480b770a5fcc0b5901e5e
mouwaficbdr <badaroumouwafic@gmail.com>
2025-06-03 21:34:39 +0100

    Réorganisation de la structure de l'application avec ajout des routes pour Home, Dashboard et Settings dans le composant App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import AuthProvider from "./features/auth/AuthProvider";
+
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Settings from "./pages/Settings";
+
 const App = () => {
   return (
-    <h1>
-      Hello World
-    </h1>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="settings" element={<Settings />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
